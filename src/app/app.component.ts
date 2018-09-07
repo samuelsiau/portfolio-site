3364ec544de96075972a8849beb58d6e6115a46e
BKA\Samuel <samuel@bka.co.nz>
2018-09-07 16:04:00 +1200

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myPortfolioApp';
+  title = 'Portfolio Website Coming Soon';
 }
