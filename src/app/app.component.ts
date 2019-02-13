a7cacc6b6cd2f229b402da6b3160f0361ee5832f
Samuel <samuel@taskee.co.nz>
2019-02-13 23:22:18 +1300

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = "Samuel - Part digital designer, part frontend developer. Building pixel perfect products, services, applications and websites fit for the user's needs.";
 }
